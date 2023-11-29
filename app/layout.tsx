5b3e3eae07d6c6317ad8b295e75aabab66de07b7
bgs-study <qkrwlgp235@gmail.com>
2023-11-29 19:49:35 +0900

    Feat: 홈화면관련 파일 복구

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import '../styles/globals.css'
+
+import { Metadata } from 'next'
+
+import Footer from '@/_component/Footer'
+import Header from '@/_component/Header'
+
+export const metadata: Metadata = {
+    title: 'Bridge',
+    description: 'our communication, bridge',
+}
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <html lang="en">
+            <body>
+                <Header></Header>
+                {children}
+                <Footer></Footer>
+            </body>
+        </html>
+    )
+}
