2c5e59f3a6c8ebdcb9b848bd4c1b3d35f92366a9
bgs-study <qkrwlgp235@gmail.com>
2023-12-02 19:29:49 +0900

    Feat: 회원가입 구현
    react-hook-form, yup으로 구현
    
    Related to: 6

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,3 +1,96 @@
+'use client'
+
+import { yupResolver } from '@hookform/resolvers/yup'
+import { signUpschema } from 'hooks/validationYup'
+import { useForm } from 'react-hook-form'
+
+export type signUpSubmitForm = {
+    name: string
+    email: string
+    password: string
+    confirmPassword: string
+    sex: string
+    address: string
+    alias?: string | undefined
+    age?: number | undefined
+}
+
 export default function Signup() {
-    return <h2>signup Content</h2>
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<signUpSubmitForm>({
+        resolver: yupResolver(signUpschema),
+    })
+
+    const onSubmit = (data: signUpSubmitForm) => {
+        console.log(data)
+    }
+    return (
+        <div className="w-1/2 my-auto mx-auto">
+            <h2 className="text-center">LET’S START BRIDGE</h2>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div>
+                    <label htmlFor="name">name</label>
+                    <input type="text" id="name" {...register('name')} />
+                    {errors.name && <div>{errors.name?.message}</div>}
+                </div>
+                <div>
+                    <label htmlFor="email">email</label>
+                    <input type="email" id="email" {...register('email')} />
+                    {errors.email && <div>{errors.email?.message}</div>}
+                </div>
+                <div>
+                    <label htmlFor="password">password</label>
+                    <input
+                        type="password"
+                        id="password"
+                        {...register('password')}
+                    />
+                    {errors.password && <div>{errors.password?.message}</div>}
+                </div>
+                <div>
+                    <label htmlFor="password">password confirm</label>
+                    <input
+                        type="password"
+                        id="confirmPassword"
+                        {...register('confirmPassword')}
+                    ></input>
+                    {errors.confirmPassword && (
+                        <div>{errors.confirmPassword?.message}</div>
+                    )}
+                </div>
+                <div>
+                    <label htmlFor="sex">sex</label>
+                    <input type="radio" id="sex" {...register('sex')}></input>
+                    {errors.sex && <div>{errors.sex?.message}</div>}
+                </div>
+                <div>
+                    <label htmlFor="address">address</label>
+                    <input
+                        type="text"
+                        id="address"
+                        {...register('address')}
+                    ></input>
+                    {errors.address && <div>{errors.address?.message}</div>}
+                </div>
+                <div>
+                    <label htmlFor="alias">alias</label>
+                    <input
+                        type="text"
+                        id="alias"
+                        {...register('alias')}
+                    ></input>
+                    {errors.alias && <div>{errors.alias?.message}</div>}
+                </div>
+                <div>
+                    <label htmlFor="age">age</label>
+                    <input type="number" id="age" {...register('age')}></input>
+                    {errors.age && <div>{errors.age?.message}</div>}
+                </div>
+                <button type="submit">회원가입 하기</button>
+            </form>
+        </div>
+    )
 }
