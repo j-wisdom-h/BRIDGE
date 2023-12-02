495e9b6c9ec648e61655a69d880bb70b1b172c35
bgs-study <qkrwlgp235@gmail.com>
2023-12-02 15:22:23 +0900

    Feat: 기본 라우트 구현

---
+++ app/api/route.ts
@@ -0,0 +1,10 @@
+// export async function GET(Request) {}
+// export async function HEAD(Request) {}
+// export async function POST(Request) {}
+// export async function PUT(Request) {}
+// export async function DELETE(Request) {}
+//  A simple GET Example
+
+export async function GET(request: Request) {
+    return new Response('This is a new API route')
+}
