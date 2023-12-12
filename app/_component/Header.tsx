4a44932a0a237987b2a91fc8c45b92bd45ac690c
bgs-study <qkrwlgp235@gmail.com>
2023-12-12 16:02:28 +0900

    Design: header position 수정

--- app/_component/Header.tsx
+++ app/_component/Header.tsx
@@ -3,15 +3,17 @@ import Link from 'next/link'
 
 const Header = () => {
     return (
-        <header className="bg-orange h-14 flexRBetween">
+        <header className="absolute top-0 right-0 left-0 bg-orange h-14 flexRBetween">
             <div className="flex self-center">
-                <Image
-                    src="/image/logo.png"
-                    width={40}
-                    height={40}
-                    alt="Picture of the logo"
-                    className="ml-1.5"
-                />
+                <Link href="/">
+                    <Image
+                        src="/image/logo.png"
+                        width={40}
+                        height={40}
+                        alt="Picture of the logo"
+                        className="ml-1.5"
+                    />
+                </Link>
             </div>
             <div className="flex mr-1.5">
                 <Link href="/signup" className="largeBoldWhiteTxt mr-1.5">
--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body className="min-h-screen relative">
+            <body className="relative">
                 <Header></Header>
                 {children}
                 <Footer></Footer>
