336b4652fe54795c48912ca9aa66f5eda9a40226
bgs-study <qkrwlgp235@gmail.com>
2023-12-13 15:25:24 +0900

    Feat: 로그인상태에 따른 헤더 Link

--- app/_component/Header.tsx
+++ app/_component/Header.tsx
@@ -1,29 +1,49 @@
+'use client'
+
 import Image from 'next/image'
 import Link from 'next/link'
+import { signOut, useSession } from 'next-auth/react'
 
 const Header = () => {
+    const { data: session } = useSession()
+
     return (
-        <header className="absolute top-0 right-0 left-0 bg-orange h-14 flexRBetween">
-            <div className="flex self-center">
+        <header className="absolute top-0 right-0 left-0 bg-orange h-14 flex items-center justify-between">
+            <div className="flex items-center ml-4">
                 <Link href="/">
                     <Image
                         src="/image/logo.png"
+                        alt="Picture of the logo"
                         width={40}
                         height={40}
-                        alt="Picture of the logo"
                         className="ml-1.5"
                     />
                 </Link>
             </div>
-            <div className="flex mr-1.5">
-                <Link href="/signup" className="largeBoldWhiteTxt mr-1.5">
-                    Signup
-                </Link>
-                <Link href="/login" className="largeBoldWhiteTxt">
-                    Login
-                </Link>
+            <div className="flex mr-4">
+                {session ? (
+                    <button
+                        onClick={() => signOut()}
+                        className="text-white mr-4"
+                    >
+                        Sign out
+                    </button>
+                ) : (
+                    <>
+                        <Link
+                            href="/signup"
+                            className="largeBoldWhiteTxt mr-1.5"
+                        >
+                            Signup
+                        </Link>
+                        <Link href="/signin" className="largeBoldWhiteTxt">
+                            Signin
+                        </Link>
+                    </>
+                )}
             </div>
         </header>
     )
 }
+
 export default Header
