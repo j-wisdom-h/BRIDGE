16693c00d4aa5177ae5dceba8a604d03ea94aa07
bgs-study <qkrwlgp235@gmail.com>
2023-12-13 15:26:33 +0900

    Comment: 불필요한 주석 삭제

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,6 @@
-export default async function Home() {
-    //const sql = 'select * from bridge.user'
-    //const data = await executeQuery(sql, '')
-    //const getdata = JSON.parse(JSON.stringify(data))
-    //console.log(getdata)
+export default function Home() {
     return (
         <div>
-            {/*
-            {getdata?.map((data, i) => {
-                return (
-                    <>
-                        <p>
-                            {data.id} {data.username}
-                        </p>
-                    </>
-                )
-            })} */}
             <p>main</p>
         </div>
     )
