f1f9f96ee0172656c1c22172dd32d0804eb1dbc5
bgs-study <qkrwlgp235@gmail.com>
2023-12-13 14:59:55 +0900

    Rename: rename app/login to app/signin

---
+++ app/signin/page.tsx
@@ -0,0 +1,12 @@
+import { LoginForm } from '@/_component/AuthForm'
+
+function Signin() {
+    return (
+        <>
+            <h2>LoginPage</h2>
+            <LoginForm />
+        </>
+    )
+}
+
+export default Signin
