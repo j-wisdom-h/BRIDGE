c4fccec143a0e56a91e1bba058f239b98f7cf8fc
JIHYE PARK <79756267+j-wisdom-h@users.noreply.github.com>
2023-12-20 16:59:12 +0900

    Feature/signup : 회원가입 구현 (#12)
    * Feat: 회원가입 구현
    
    react-hook-form 회원가입 폼 구현
    yup으로 유효성 검사
    회원가입API
    회원가입 완료 전, 가입된 이메일인지 확인하는 기능 구현
    nodemailer 이메일 인증, 인증번호 비교 API 
    
    Related to: #10

---
+++ app/preload-resources.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import ReactDOM from 'react-dom/client'
+
+export function PreloadResources() {
+    const head = ReactDOM.createRoot(document.head)
+
+    head.render(
+        <link
+            rel="preload"
+            href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable.min.css"
+        />,
+    )
+    //head.render(<link rel='preconnect' href='...' />)
+    //head.render(<link rel='prefetchDNS' href='...' />)
+
+    return null
+}
