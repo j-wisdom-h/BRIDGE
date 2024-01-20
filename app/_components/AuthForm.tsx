d5f6d7788790975ec45b18e81ad4d968868cc713
bgs-study <qkrwlgp235@gmail.com>
2024-01-20 15:53:55 +0900

    Feat : 구글 로그인 시 사용자 정보 표시 및 로그인 실패시 메세지 경고창

--- app/_components/AuthForm.tsx
+++ app/_components/AuthForm.tsx
@@ -3,6 +3,7 @@
 import { yupResolver } from '@hookform/resolvers/yup'
 import { logInschema, signUpschema } from 'hooks/validationYup'
 import { signIn } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
 import { useRef, useState } from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
 
@@ -10,6 +11,7 @@ import { SignInput } from '@/_components/Input'
 import { IsignUp, User } from '@/_interfaces/IAuth'
 
 function LoginForm() {
+    const router = useRouter()
     const {
         register,
         handleSubmit,
@@ -21,41 +23,57 @@ function LoginForm() {
 
     const onSubmit = async (data: IsignUp) => {
         const { email, password } = data
-        await signIn('credentials', {
+
+        const res = await signIn('credentials', {
             username: email,
             password: password,
-            redirect: true,
+            redirect: false,
             callbackUrl: '/',
         })
+        // 에러 핸들링
+        if (res?.status === 401) {
+            alert('아이디 혹은 비밀번호가 일치하지 않습니다!')
+        } else {
+            router.push('/')
+        }
     }
 
     return (
-        <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="border-solid border px-8 py-12"
-        >
-            <div>
+        <>
+            <form
+                onSubmit={handleSubmit(onSubmit)}
+                className="border-solid border px-8 py-12"
+            >
                 <div>
-                    <label htmlFor="email">email</label>
-                    <SignInput type="text" id="email" {...register('email')} />
+                    <div>
+                        <label htmlFor="email">email</label>
+                        <SignInput
+                            type="text"
+                            id="email"
+                            {...register('email')}
+                        />
+                    </div>
+                    {errors.email && <div>{errors.email?.message}</div>}
                 </div>
-                {errors.email && <div>{errors.email?.message}</div>}
-            </div>
-            <div>
                 <div>
-                    <label htmlFor="password">password</label>
-                    <SignInput
-                        type="password"
-                        id="password"
-                        {...register('password')}
-                    />
+                    <div>
+                        <label htmlFor="password">password</label>
+                        <SignInput
+                            type="password"
+                            id="password"
+                            {...register('password')}
+                        />
+                    </div>
+                    {errors.password && <div>{errors.password?.message}</div>}
                 </div>
-                {errors.password && <div>{errors.password?.message}</div>}
-            </div>
-            <button type="submit" className="orangeBtnL">
-                SignIn
+                <button type="submit" className="orangeBtnL">
+                    SignIn
+                </button>
+            </form>
+            <button onClick={() => signIn('google', { callbackUrl: '/' })}>
+                구글로그인
             </button>
-        </form>
+        </>
     )
 }
 
@@ -155,30 +173,33 @@ function SignUpForm() {
     return (
         <form
             onSubmit={handleSubmit(onSubmit)}
-            className="min-w-[600px] w-full max-w-md h-[700px] bg-white rounded-2xl shadow-md p-10 border-8 border-orange text-black [&>*:not(:last-child)]:mb flex justify-center flex-col"
+            className="min-w-[400px] w-full max-w-md h-[500px] bg-white rounded-2xl shadow-md p-10 border-2 border-darkgray text-black [&>*:not(:last-child)]:mb flex justify-center flex-col"
         >
-            <div className="mb-2 h-12">
-                <div className="flexRBetween">
-                    <label htmlFor="email" className="basis-1/3 text-lg">
-                        email
-                    </label>
-                    <div className="basis-2/3 flex max-w-[66.666667%]">
-                        <SignInput
-                            type="text"
-                            id="email"
-                            placeholder="Email"
-                            className="text-lg"
-                            {...register('email')}
-                        />
-                        <button
-                            type="button"
-                            className="orangeBtnS"
-                            onClick={handleVerification}
-                        >
-                            메일인증
-                        </button>
+            <div className="mb-2">
+                <label className="form-control w-full max-w-xs relative">
+                    <div className="label pb-0">
+                        <span className="label-text">email</span>
                     </div>
-                </div>
+                    <input
+                        type="text"
+                        id="email"
+                        placeholder="Email"
+                        className="input input-bordered input-sm w-full max-w-xs"
+                        {...register('email')}
+                    />
+                    <button
+                        type="button"
+                        className="absolute top-7 right-0 bg-orange text-white rounded-md w-16 text-xs h-8"
+                        onClick={handleVerification}
+                    >
+                        메일인증
+                    </button>
+                    <div className="label h-4 pb-0">
+                        <span className="label-text-alt">
+                            {errors.email && errors.email?.message}
+                        </span>
+                    </div>
+                </label>
                 {showVerifyInput && (
                     <div>
                         <input
@@ -199,42 +220,53 @@ function SignUpForm() {
                         </button>
                     </div>
                 )}
-                {errors.email && <p>{errors.email?.message}</p>}
             </div>
-            <div className="mb-2 h-12">
-                <div className="flexRBetween">
-                    <label htmlFor="password" className="basis-1/3 text-lg">
-                        password
-                    </label>
+            <div className="mb-2">
+                <label
+                    htmlFor="password"
+                    className="form-control w-full max-w-xs"
+                >
+                    <div className="label pb-0">
+                        <span className="label-text">password</span>
+                    </div>
                     <SignInput
                         type="password"
                         id="password"
-                        className="w-full text-lg"
+                        className="input input-bordered input-sm w-full max-w-xs"
                         placeholder="Password"
                         {...register('password')}
                     />
-                </div>
-                {errors.password && <p>{errors.password?.message}</p>}
+                    <div className="label h-4 pb-0">
+                        <span className="label-text-alt">
+                            {errors.password && errors.password?.message}
+                        </span>
+                    </div>
+                </label>
             </div>
 
-            <div className="mb-2 h-12">
-                <div className="flexRBetween">
-                    <label htmlFor="password" className="basis-1/3 text-lg">
-                        password confirm
-                    </label>
+            <div className="mb-2">
+                <label
+                    htmlFor="password"
+                    className="form-control w-full max-w-xs"
+                >
+                    <div className="label pb-0">
+                        <span className="label-text">password confirm</span>
+                    </div>
                     <SignInput
-                        type="password"
                         id="confirmPassword"
-                        className="text-lg"
+                        type="password"
+                        className="input input-bordered input-sm w-full max-w-xs"
                         placeholder="Password"
                         {...register('confirmPassword')}
                     />
-                </div>
-                {errors.confirmPassword && (
-                    <p>{errors.confirmPassword?.message}</p>
-                )}
+                    <div className="label h-4 pb-0">
+                        <span className="label-text-alt">
+                            {errors.confirmPassword &&
+                                errors.confirmPassword?.message}
+                        </span>
+                    </div>
+                </label>
             </div>
-
             <button type="submit" className="orangeBtnL w-full mt-12">
                 SignUp
             </button>
--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,10 +1,21 @@
 import 'next-auth'
 
-declare module 'next-auth' {
-    interface Session {
-        user: {
-            email: string
-            accessToken: string
-        }
-    }
-}
+// declare module 'next-auth' {
+//     interface Session {
+//         user: {
+//             email: string
+//             accessToken: string
+//         }
+//     }
+// }
+// declare module 'next-auth/jwt' {
+//     interface JWT {
+//         profile: {
+//             sub?: string
+//             name?: string
+//             email?: string
+//             image?: string
+//             picture?: string
+//         }
+//     }
+// }
