ae323599da54594412776a935b5ea86c17b06939
bgs-study <qkrwlgp235@gmail.com>
2024-01-20 16:10:26 +0900

    Design: 회원가입 link 추가

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,10 +1,18 @@
-import { LoginForm } from '@/_component/AuthForm'
+import Link from 'next/link'
+
+import { LoginForm } from '@/_components/AuthForm'
 
 function Signin() {
     return (
         <>
             <h2>LoginPage</h2>
             <LoginForm />
+            <Link
+                href="/signup"
+                className="underline underline-offset-1 text-gray-400"
+            >
+                Sign up
+            </Link>
         </>
     )
 }
