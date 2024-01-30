c1b2c6319a88f85f7414da29b7404e8efd2b6789
bgs-study <qkrwlgp235@gmail.com>
2024-01-30 16:07:19 +0900

    Fix: 쿼리 실행함수 제네릭 타입 지정

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -6,11 +6,10 @@ import { NextResponse } from 'next/server'
 export async function POST(request: Request) {
     const body = await request.json()
     const sql = `select id, email, password from bridge.user where email = '${body.username}'`
-    const users: RowDataPacket[] = await executeQuery(sql, [])
+    const users = await executeQuery<RowDataPacket[]>(sql, [])
     const user = users[0]
 
     if (user && (await bcrypt.compare(body.password, user.password))) {
-        // console.log(user)
         const { password, ...userWithoutPass } = user
         return NextResponse.json(userWithoutPass)
     } else {
