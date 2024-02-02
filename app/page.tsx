d08ce64517284d3a96392c030656b8558967bda2
bgs-study <qkrwlgp235@gmail.com>
2024-02-02 13:35:01 +0900

    Design: 홈화면에 보이는 게시글 UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Link from 'next/link'
 
 import { getAllPosts } from './_lib/post'
@@ -6,14 +7,41 @@ export default async function Home() {
     const posts = await getAllPosts()
 
     return (
-        <div className="pt-12">
+        <div className="grid grid-cols-4 gap-4 auto-rows-fr p-16">
             {posts.map((post) => {
                 return (
-                    <li key={post.id} className="block mb-5">
-                        <Link href={`read/${post.id}`}>
-                            <p>제목{post.title}</p>
-                            <p>내용{post.content}</p>
-                            <p>작성자{post.email}</p>
+                    <li
+                        key={post.id}
+                        className="card bg-base-100 border border-gray-300 shadow-lg"
+                    >
+                        <Link
+                            href={`read/${post.id}`}
+                            className="card-body p-4"
+                        >
+                            <h3 className="card-title text-lg">{post.title}</h3>
+                            <div className="grow text-xs">
+                                <div>
+                                    <p className="text-ellipsis">
+                                        {post.content}
+                                    </p>
+                                </div>
+                                <div>키워드</div>
+                                <div>모집인원 : {post.num}</div>
+                                <div>
+                                    모집기간 : {post.startDate} ~ {post.endDate}
+                                </div>
+                                <div>
+                                    <span>작성자 : </span>
+                                    <span>{post.email}</span>
+                                    <Image
+                                        width={45}
+                                        height={45}
+                                        src={post.avator || '/image/logo.png'}
+                                        quality={100}
+                                        alt="avatar"
+                                    />
+                                </div>
+                            </div>
                         </Link>
                     </li>
                 )
