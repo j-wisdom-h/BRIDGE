a8c6f5aaf0715fd63a1a93c9ec3aa79002529b08
bgs-study <qkrwlgp235@gmail.com>
2024-02-02 14:00:53 +0900

    Feat: 게시글 생성, 수정
    Related to: #16

--- app/_lib/actions.ts
+++ app/_lib/actions.ts
@@ -1,10 +1,14 @@
 'use server'
+import { authOptions } from 'app/api/auth/[...nextauth]/route'
 import { ResultSetHeader } from 'mysql2'
 import { redirect } from 'next/navigation'
+import { getServerSession } from 'next-auth'
 
 import executeQuery from './db'
 
-async function createPost(userId, formdata: FormData) {
+async function formDateValues(formdata: FormData) {
+    const session = await getServerSession(authOptions)
+    const userId = session?.user?.id
     const desiredKeyOrder = [
         'title',
         'content',
@@ -23,7 +27,11 @@ async function createPost(userId, formdata: FormData) {
         return String(value)
     })
     values.push(Number(userId))
+    return values
+}
 
+async function createPost(formdata: FormData) {
+    const values = await formDateValues(formdata)
     const sql =
         'INSERT INTO bridge.post (title, content, num, location, startDate, atTime, duration, endDate, author_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)'
     let postId: number | null = null
@@ -35,4 +43,18 @@ async function createPost(userId, formdata: FormData) {
     }
     redirect(`/read/${postId}`)
 }
-export { createPost }
+
+async function updatePost(postId, formdata: FormData) {
+    const values = await formDateValues(formdata)
+    values[values.length - 1] = postId
+    const sql =
+        'UPDATE bridge.post SET title = ?, content = ?, num = ?, location = ?, startDate = ?, atTime = ?, duration = ?, endDate = ? WHERE id = ?'
+    try {
+        await executeQuery<ResultSetHeader>(sql, [...values])
+    } catch (err) {
+        throw new Error('포스터 수정 실패')
+    }
+    redirect(`/read/${postId}`)
+}
+
+export { createPost, updatePost }
