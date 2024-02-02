3388a5b0b386189d1140bfac7fa55e71f5cc8c19
bgs-study <qkrwlgp235@gmail.com>
2024-02-02 14:05:54 +0900

    Fix: 홈화면에서 가져오는 게시글 오름차순, 개수 제한

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,11 +1,16 @@
 import executeQuery from 'app/_lib/db'
 import { RowDataPacket } from 'mysql2'
 import { NextRequest, NextResponse } from 'next/server'
+import { dateFormatting } from 'utils/date'
 
 export async function GET(request: NextRequest) {
+    const limit = 16
     const sql = `SELECT *
-                FROM bridge.user
-                JOIN bridge.post ON bridge.user.id = bridge.post.author_id`
+                    FROM bridge.user
+                    JOIN bridge.post ON bridge.user.id = bridge.post.author_id ORDER BY created_at DESC LIMIT ${limit}`
     const posts = await executeQuery<RowDataPacket[]>(sql, [])
-    return NextResponse.json(posts)
+
+    const transformedPosts = dateFormatting(posts)
+
+    return NextResponse.json(transformedPosts)
 }
