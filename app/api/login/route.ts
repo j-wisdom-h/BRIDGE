00052ab8255bc735e5bbd2c511e5356dd4b29ffe
bgs-study <qkrwlgp235@gmail.com>
2024-02-02 14:05:01 +0900

    Fix: 로그인시 accessToken 추가

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,4 +1,5 @@
 import executeQuery from 'app/_lib/db'
+import { signJwtAccessToken } from 'app/_lib/jwt'
 import bcrypt from 'bcrypt'
 import { RowDataPacket } from 'mysql2'
 import { NextResponse } from 'next/server'
@@ -11,7 +12,9 @@ export async function POST(request: Request) {
 
     if (user && (await bcrypt.compare(body.password, user.password))) {
         const { password, ...userWithoutPass } = user
-        return NextResponse.json(userWithoutPass)
+        const accessToken = signJwtAccessToken(userWithoutPass)
+        const result = { ...userWithoutPass, accessToken }
+        return NextResponse.json(result)
     } else {
         throw new Error('No member information found')
     }
