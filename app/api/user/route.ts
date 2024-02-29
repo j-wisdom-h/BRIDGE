acf63b2295f1b868e8a81156adb71dbf8a5721ce
bgs-study <qkrwlgp235@gmail.com>
2024-02-29 12:27:55 +0900

    Fix: 회원가입시 알람테이블 생성

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -23,12 +23,23 @@ export async function signUpUser(user: User) {
         }
 
         const result = await executeQuery<ResultSetHeader>(sql, values)
+        const userId = result.insertId
         console.log('User registered:', result)
-        return { success: true, message: 'User registered successfully' }
+        return {
+            userId: userId,
+            success: true,
+            message: 'User registered successfully',
+        }
     } catch (error) {
         console.error('Error registering user:', error)
         return { success: false, message: 'Error registering user' }
     }
+}
+
+// 사용자 알람 테이블에 새로운 row를 생성하는 함수
+export async function createAlarmRow(userId: number) {
+    const sql = 'INSERT INTO alarm (user_id, checked_count) VALUES (?, ?)'
+    await executeQuery<ResultSetHeader>(sql, [userId, 0]) // 처음에 확인한 개수는 0으로 초기화
 }
 
 // POST 요청 처리
@@ -41,6 +52,15 @@ export async function POST(request: Request) {
     }
 
     const result = await signUpUser(user)
+
+    if (result.success && result.userId) {
+        // 사용자 알람 테이블 row생성
+        try {
+            await createAlarmRow(result.userId) // 알람 테이블에 새로운 row 생성
+        } catch (error) {
+            console.error('Error creating alarm row for user:', error)
+        }
+    }
 
     return NextResponse.json(JSON.stringify(result), {
         headers: { 'Content-Type': 'application/json' },
