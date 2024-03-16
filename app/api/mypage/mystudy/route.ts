e3ba5f85ff36a04e271f3d7cbfab27a7cfc1dc57
bgs-study <qkrwlgp235@gmail.com>
2024-03-16 17:01:56 +0900

    Fix: mystudy GET API 확장
    기존 userId로 post테이블을 탐색하던 기능을 user_id로 study테이블을
    탐색하게 변경(불필요한 데이터를 가져오는 것을 막음)
    studyId params를 추가해 IntrodutionTab에서 가져올 때 GET API 재사용

--- app/api/mypage/mystudy/route.ts
+++ app/api/mypage/mystudy/route.ts
@@ -5,13 +5,36 @@ import executeQuery from '@/_lib/db'
 
 export async function GET(req: NextRequest) {
     const userId = req.nextUrl.searchParams.get('userId')
-    const sql = `SELECT *
-        FROM post
-        JOIN study ON post.id = study.post_id
-        JOIN study_member ON study.id = study_member.study_id
-        JOIN member ON member.id = study_member.member_id
-        WHERE member.user_id = ?    
-    `
-    const res = await executeQuery<RowDataPacket[]>(sql, [userId])
+    const studyId = req.nextUrl.searchParams.get('studyId')
+    let params, sql
+    if (userId) {
+        params = userId
+        sql = `
+            SELECT
+            s.id,
+            s.title
+            FROM study s
+            INNER JOIN study_member sm ON s.id = sm.study_id
+            INNER JOIN member m ON sm.member_id = m.id
+            WHERE m.user_id = ?`
+    }
+    if (studyId) {
+        params = studyId
+        sql = `SELECT
+        study.id,
+        post.id AS post_id,
+        post.title,
+        post.content AS post_content,
+        post.created_at AS post_created_at,
+        post.updated_at AS post_updated_at,
+        post.keywords,
+        post.location AS post_location,
+        post.num AS post_num,
+        post.atTime AS post_atTime
+        FROM study
+        LEFT JOIN post ON study.post_id = post.id
+        WHERE study.id = ?`
+    }
+    const res = await executeQuery<RowDataPacket[]>(sql, [params])
     return NextResponse.json(res)
 }
