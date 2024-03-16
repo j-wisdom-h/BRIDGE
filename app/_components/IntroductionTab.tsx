80994f720795adb7aad5c676236b85999f5df2dd
bgs-study <qkrwlgp235@gmail.com>
2024-03-16 17:22:51 +0900

    Feat: IntroductionTab 지연시간에 따른 노출 스켈레톤

--- app/_components/IntroductionTab.tsx
+++ app/_components/IntroductionTab.tsx
@@ -1,13 +1,62 @@
-export default function IntroductionTab({ postInfo }) {
-    // 기간, 현재인원수, 아이콘
-    const { title, content, location, num, atTime } = postInfo
+'use client'
+
+import { useEffect, useState } from 'react'
+
+import { IstudyAll } from '@/_interfaces/IStudy'
+import { getMembers, getStudy } from '@/_lib/study'
+
+import DeferredComponent from './DeferredComponent'
+import IntroductionContent from './IntroductionContent'
+
+const Skeleton = () => {
     return (
-        <div className="card bg-base-100 shadow-xl h-52 p-5">
-            <p> titel : {title}</p>
-            <p> content : {content}</p>
-            <p> location : {location}</p>
-            <p> num : {num}</p>
-            <p> atTime : {atTime}</p>
+        <div className="flex flex-col gap-2 w-full">
+            <div className="skeleton h-4 w-full"></div>
+            <div className="skeleton h-4 w-full"></div>
+            <div className="skeleton h-4 w-full"></div>
+            <div className="skeleton h-4 w-full"></div>
+            <div className="skeleton h-4 w-full"></div>
+            <div className="avatar-group -space-x-6 rtl:space-x-reverse overflow-visible">
+                <div className="skeleton w-12 h-12 rounded-full shrink-0"></div>
+                <div className="skeleton w-12 h-12 rounded-full shrink-0"></div>
+                <div className="skeleton w-12 h-12 rounded-full shrink-0"></div>
+                <div className="skeleton w-12 h-12 rounded-full shrink-0"></div>
+            </div>
         </div>
     )
 }
+
+const fetchData = async (studyId: number) => {
+    const [study, members] = await Promise.all([
+        getStudy(studyId),
+        getMembers(studyId),
+    ])
+    return [study, members] as IstudyAll
+}
+
+function IntroductionTab({ studyId }) {
+    const [studyInfo, setStudyInfo] = useState<null | IstudyAll>(null)
+
+    useEffect(() => {
+        async function loadData() {
+            try {
+                const data = await fetchData(studyId)
+                setStudyInfo(data)
+            } catch (e) {
+                console.error(e)
+            }
+        }
+        loadData()
+    }, [])
+
+    if (!studyInfo) {
+        return (
+            <DeferredComponent>
+                <Skeleton />
+            </DeferredComponent>
+        )
+    }
+    return <IntroductionContent studyInfo={studyInfo} />
+}
+
+export default IntroductionTab
